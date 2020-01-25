35e5b8811cd0dc4300b2edb0e1505dfd2b9ae600
morita-daiki <37872526+Morita-Daiki@users.noreply.github.com>
2020-01-25 23:34:32 +0900

    コメント修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 #include <mbed.h>
+#if defined(DEVICE_CAN) || defined(DOXYGEN_ONLY)
 
 DigitalOut led_r(PB_4); //Red
 DigitalOut led_g(PB_5); //Green
 DigitalOut led_b(PB_0); //Blue
 
-DigitalOut in1(PA_2);    //CW
-DigitalOut in2(PA_3);    //CCW
+PwmOut in1(PA_2);
+PwmOut in2(PA_3);
+// DigitalOut in1(PA_2);    //CW
+// DigitalOut in2(PA_3);    //CCW
 DigitalOut sleepn(PA_6); //HIGHで駆動可能，LOWで空転
 DigitalIn ocln(PA_5);    //目標電流 < 実電流
 AnalogOut vref(PA_4);    //目標電流
-AnalogIn volage(PA_1);   //現在電圧
+AnalogIn volage(PA_1);   //現在電圧(Vpower*10k/110k)
 AnalogIn aiout(PA_7);    //現在電流
 
 InterruptIn enc_a(PA_8); //エンコーダA
-InterruptIn enc_b(PA_9); //エンコーダB
+DigitalIn enc_b(PA_9);
+// InterruptIn enc_b(PA_9); //エンコーダB
 
 AnalogIn id_setting(PA_0);      //setting
 CAN can(PA_11, PA_12, 1000000); //CAN
@@ -33,7 +37,7 @@ int id; //0~7
 double idsumval = 0.0;
 char read_data[READ_LEN];
 char send_data[SEND_LEN];
-
+long encoder_val = 0;
 int set_id()
 {
   double id_sum = 0.0;
@@ -49,31 +53,67 @@ void init()
   led_r = (id >> 0) & 1;
   led_g = (id >> 1) & 1;
   led_b = (id >> 2) & 1;
+  wait(0.5);
+
+  sleepn = 1;
+  in1 = in2 = 1;
+
+  can_stby = 0;
+
+  // wait(1.0);
+  // led_r = 1;
+  // wait(1.0);
 }
-void send()
+void set_current(double current) //if torqu=0 then break mode
 {
-  if (can.write(CANMessage(id + POSITION_OFFSET, send_data, SEND_LEN)))
-  {
-    // led_r = !led_r; //blink red
-  }
+  vref = min(abs(current), VREF_LIMIT); //vref=電流*Rsense*10
+  in1 = current >= 0;                   //cw
+  in2 = current <= 0;                   //ccw
 }
 
+void send()
+{
+  led_r = !led_r;
+  if (can.write(CANMessage(id + POSITION_OFFSET, send_data, SEND_LEN)))
+  {
+    led_g = !led_g; //blink red
+  }
+}
+void encoder_rise()
+{
+  encoder_val += !enc_b - enc_b;
+}
+void encoder_fall()
+{
+  encoder_val += enc_b - !enc_b;
+}
 int main()
 {
   init();
-  ticker.attach(&send, 0.01);
+  enc_a.rise(&encoder_rise);
+  enc_a.fall(&encoder_fall);
+  ticker.attach(&send, 0.1); //can send 1 [data/ms]
 
   CANMessage msg;
   while (1)
   {
-    init();
+    set_current(0.01);
+    led_b = !led_b;
+    wait(5);
+    set_current(-0.01);
+    led_b = !led_b;
+    wait(5);
+    // init();
     if (can.read(msg))
     {
       if ((int)msg.id == (id + CURRENT_OFFSET))
       {
-        // led_g = !led_g; //blink red
+        led_g = !led_g; //blink red
         target_current = (msg.data[1] << 8 | msg.data[0]);
       }
     }
   }
 }
+#else
+#error CAN NOT SUPPORTED
+#endif
